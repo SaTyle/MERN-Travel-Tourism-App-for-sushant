5661e8e559a5c3c26394d3078388c25c51a09be2
SaTyle <zxcvwifi@gmail.com>
2024-04-17 20:01:59 +0530

    -

--- backend/controllers/package.controller.js
+++ backend/controllers/package.controller.js
@@ -6,10 +6,14 @@ dotenv.config();
 
 //payment gateway
 var gateway = new braintree.BraintreeGateway({
-  environment: braintree.Environment.Sandbox,
-  merchantId: process.env.BRAINTREE_MERCHANT_ID,
-  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+  // environment: braintree.Environment.Sandbox,
+  // merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  // publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  // privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+  environment: "noEnv",
+  merchantId:  123 ,
+  publicKey: '456' ,
+  privateKey: 132465 ,
 });
 
 //create package
